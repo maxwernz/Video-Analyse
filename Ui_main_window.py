3d43d1c0f500c1da3cb733ffb175bf552de4a473
Max Wernz <mawernz@outlook.de>
2023-11-11 18:56:03 +0100

    Window title to "Video Analyse"

--- Ui_main_window.py
+++ Ui_main_window.py
@@ -145,7 +145,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Video Analyse"))
         self.treeWidget.setSortingEnabled(True)
         self.treeWidget.headerItem().setText(0, _translate("MainWindow", "Clip"))
         self.treeWidget.headerItem().setText(1, _translate("MainWindow", "Start"))
